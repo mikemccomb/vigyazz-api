8f969c52ecdbfccec89980b9746345b40adf3114
mikemccomb <mikemccomb@gmail.com>
2023-05-08 14:28:55 -0500

    Adds more users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-User.create(first_name: "Mike", last_name: "McComb", email: "mike@example.com", password: "password", password_confirmation: "password", home_currency: "USD")
+User.create(first_name: "Yulia", last_name: "Kobov", email: "yulia@example.com", password: "password", password_confirmation: "password", home_currency: "UAH")
+User.create(first_name: "Dave", last_name: "Steeves", email: "dave@example.com", password: "password", password_confirmation: "password", home_currency: "USD")
+User.create(first_name: "Ann", last_name: "Gav", email: "ann@example.com", password: "password", password_confirmation: "password", home_currency: "GBP")
+User.create(first_name: "Paul", last_name: "Losso", email: "paul@example.com", password: "password", password_confirmation: "password", home_currency: "EUR")
