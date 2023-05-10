1cf29d3a08020d075b4137282f1d19634ae3ba2d
mikemccomb <mikemccomb@gmail.com>
2023-05-10 16:12:43 -0500

    Updates controller for query functionality

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,8 @@ require "json"
 
 class WeatherController < ApplicationController
   def index
-    response = HTTP.get("http://api.weatherapi.com/v1/current.json?key=#{ENV["WEATHER_API_KEY"]}&q=boston")
+    query = params["search"]
+    response = HTTP.get("http://api.weatherapi.com/v1/current.json?key=#{ENV["WEATHER_API_KEY"]}&q=#{query}")
     data = JSON.parse(response)
     render json: data
   end
