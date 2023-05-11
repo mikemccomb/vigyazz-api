33dff8797d3c170c1385f80d36b7c521e1d3ebaa
mikemccomb <mikemccomb@gmail.com>
2023-05-11 15:46:32 -0500

    Comments out seeds list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,23 @@
+# ---- Dummy users ----
+
 # User.create(first_name: "Yulia", last_name: "Kobov", email: "yulia@example.com", password: "password", password_confirmation: "password", home_currency: "UAH")
 # User.create(first_name: "Dave", last_name: "Steeves", email: "dave@example.com", password: "password", password_confirmation: "password", home_currency: "USD")
 # User.create(first_name: "Ann", last_name: "Gav", email: "ann@example.com", password: "password", password_confirmation: "password", home_currency: "GBP")
 # User.create(first_name: "Paul", last_name: "Losso", email: "paul@example.com", password: "password", password_confirmation: "password", home_currency: "EUR")
 
-require "csv"
+# ---- Currency List Seed ----
 
-csv_text = File.read(Rails.root.join("lib", "seeds", "currency-list.csv"))
-csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
-csv.each do |row|
-  c = Currency.new
-  c.entity = row["Entity"]
-  c.currency = row["Currency"]
-  c.alphabetic_code = row["AlphabeticCode"]
-  c.save
-  puts "#{c.entity}: #{c.currency} and #{c.alphabetic_code} saved"
-end
+# require "csv"
 
-puts "There are now #{Currency.count} rows in the Currency model"
+# csv_text = File.read(Rails.root.join("lib", "seeds", "currency-list.csv"))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+# csv.each do |row|
+#   c = Currency.new
+#   c.entity = row["Entity"]
+#   c.currency = row["Currency"]
+#   c.alphabetic_code = row["AlphabeticCode"]
+#   c.save
+#   puts "#{c.entity}: #{c.currency} and #{c.alphabetic_code} saved"
+# end
+
+# puts "There are now #{Currency.count} rows in the Currency model"
