ed6db693689ded9dd896668ada8a388399fa0d7f
mikemccomb <mikemccomb@gmail.com>
2023-05-11 15:44:19 -0500

    Adds currency model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_173553) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_11_203547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "entity"
+    t.string "currency"
+    t.string "alphabetic_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
