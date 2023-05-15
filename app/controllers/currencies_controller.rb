d72fe6d56f6ffe08552e591365b175b38d90c338
mikemccomb <mikemccomb@gmail.com>
2023-05-15 15:43:54 -0500

    Adds code to attempt API call (commented out)

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -8,6 +8,14 @@ class CurrenciesController < ApplicationController
 
   def show
     currency = Currency.find_by(entity: params[:entity])
+
+    # if currency
+    #   # currency.alphabetic_code
+    #   shortcode = "EUR"
+    #   # response = HTTP.get("https://api.currencyapi.com/v3/latest?apikey=#{ENV["CURRENCY_API_KEY"]}&currencies=#{shortcode}")
+    #   data = JSON.parse(response)
+    #   currency[:conversion] = data
+    # end
     render json: currency
   end
 end
