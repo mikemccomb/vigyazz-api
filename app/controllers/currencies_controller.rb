5294d9fa50cf86c53d31bcf73bbdb543e2ac9fe1
mikemccomb <mikemccomb@gmail.com>
2023-05-15 15:05:52 -0500

    Restores Index Action

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,6 +1,11 @@
 require "json"
 
 class CurrenciesController < ApplicationController
+  def index
+    @currencies = Currency.all
+    render :index
+  end
+
   def show
     currency = Currency.find_by(entity: params[:entity])
     render json: currency
